ef60afaf0816e594c2bebe7457680a6e510b25c5
ElkynEnriquez <efenriquezm@gmail.com>
2017-06-01 17:48:17 -0500

    Update autenticar.php

--- autenticar.php
+++ autenticar.php
@@ -1,3 +1,17 @@
+<script language="JavaScript" src="js/md5.js"></script>
+<script language="JavaScript">
+numero = Math.random().toString();
+function calculaMD5() {
+var pw = document.forms["login"].elements["password"].value
+pw += numero
+return hex_md5(pw)
+}
+function enviaMD5(hash) {
+document.forms["login"].elements["cifrado"].value = hash;
+document.forms["login"].elements["numero"].value = numero;
+document.forms["login"].submit();
+}
+</script>
 <div class="container">
   <div class="row">
   	<div class="col s1 m4 l4"> </div>
@@ -7,7 +21,9 @@
 	</div>
   <div class="row">
   	<div class="col s12 m3 l4"> </div>
-    <form class="col s12 m6 l4 indigo darken-3" action="conexion.php" method="post" enctype="multipart/form-data"> 
+    <form name="login" class="col s12 m6 l4 indigo darken-3" action="conexion.php" method="post" enctype="multipart/form-data">
+    	<input type="Hidden" name="cifrado" value="">
+		<input type="Hidden" name="numero" value=""> 
       <div class="row"><br />
         <div class="input-field col s12">
           <input id="usuario" name="usuario" type="text" class="validate">
@@ -21,7 +37,7 @@
         </div>
       </div>
       <div class="row"><div class="col s11">
-      <button class="btn-floating btn-large waves-effect waves-light green right" type="submit" name="action">
+      <button class="btn-floating btn-large waves-effect waves-light green right" type="submit" name="action" onClick="enviaMD5(calculaMD5())">
 	   	<i class="material-icons">done</i>
   		</button>
   		</div></div>
