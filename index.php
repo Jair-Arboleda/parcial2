51499e9c17a45751f4851d78bef177e5a3354c1b
ElkynEnriquez <efenriquezm@gmail.com>
2017-06-01 15:42:09 -0500

    Update index.php

--- index.php
+++ index.php
@@ -9,11 +9,30 @@
    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 </head>
 <body class="indigo darken-4" style="display: flex; min-height: 100vh; flex-direction: column;">
-<?php
-	require('autenticar.php');
-?>
-   <!--Import jQuery before materialize.js-->
+
+	<!--Importando jQuery y materialize.js-->
    <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
    <script type="text/javascript" src="js/materialize.min.js"></script>
+
+<!-- ----------------------------div para cargar paginas-->   
+<main style="flex: 1 0 auto;"><div class="row">
+	<br><br><br class='hide-on-med-and-up'><br class='hide-on-med-and-up'>
+   	<?php
+	   	if(isset ($_GET['pag'])){
+	     		if ($_GET['pag']==1) require ("autenticar.php");
+		   	elseif ($_GET['pag']==2) require ("asignaturas.php");
+		   	elseif ($_GET['pag']==3)require ("eliminar.php");
+		   	elseif ($_GET['pag']==4)require ("modificar.php");
+		  	}
+		   else require ("autenticar.php");
+      ?>
+</div></main>
+<footer class="page-footer blue-grey darken-1">
+	<div class="footer-copyright">
+   	<div class="container">
+      	Junio 2017 Elkyn Enriquez - Jair Arboleda
+      </div>
+   </div>
+</footer>
 </body>
 </html>
