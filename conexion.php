e6698b9c14f5eb4c177ad3668f83e1e3cd41c66e
ElkynEnriquez <efenriquezm@gmail.com>
2017-06-01 15:40:19 -0500

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -3,14 +3,17 @@ session_start();
 if (($_POST['usuario']=="colegio") && ($_POST['password']=="123")) {
     $_SESSION['Susuario'] = $_POST["usuario"];
     $_SESSION['Spassword'] = $_POST["password"];
-    echo "Bienvenido <meta http-equiv='refresh' content='5; URL=indx.php' />";
+    echo "<body bgcolor='161D29' text='ddddff'><center><br><br><h1>Bienvenido</h1><img src='leon.gif'></center></body>
+  		  	 <meta http-equiv='refresh' content='5; URL=index.php?pag=1' />";
 	$conex=mysqli_connect("localhost",$_POST["usuario"],$_POST["password"],"colegio");
 } 
 elseif ($_POST['usuario']=="Admincol" && $_POST['password']=="321") {
     $_SESSION['Susuario'] = $_POST["usuario"];
     $_SESSION['Spassword'] = $_POST["password"];
-    echo "Bienvenido <meta http-equiv='refresh' content='5; URL=lisr.php' />";
+    echo "<body bgcolor='161D29' text='ddddff'><center><br><br><h1>Bienvenido</h1><img src='leon.gif'></center></body>
+    <meta http-equiv='refresh' content='5; URL=index.php?pag=1' />";
 	$conex=mysqli_connect("localhost",$_POST["usuario"],$_POST["password"],"colegio");
 } 
-else echo "Usuario no válido <meta http-equiv='refresh' content='5;URL=autenticar.php' />";
+else echo "<body bgcolor='000000'><br><br><center><img src='error.gif'></center><body>
+<meta http-equiv='refresh' content='5;URL=index.php?pag=1' />";
 ?>
