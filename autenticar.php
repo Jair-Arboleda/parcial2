063d96af545b430b260a2a318b468113fe8bbd32
ElkynEnriquez <efenriquezm@gmail.com>
2017-06-01 23:49:03 -0500

    Update autenticar.php

--- autenticar.php
+++ autenticar.php
@@ -1,46 +1,22 @@
-<script language="JavaScript" src="js/md5.js"></script>
-<script language="JavaScript">
-numero = Math.random().toString();
-function calculaMD5() {
-var pw = document.forms["login"].elements["password"].value
-pw += numero
-return hex_md5(pw)
-}
-function enviaMD5(hash) {
-document.forms["login"].elements["cifrado"].value = hash;
-document.forms["login"].elements["numero"].value = numero;
-document.forms["login"].submit();
-}
-</script>
-<div class="container">
-  <div class="row">
-  	<div class="col s1 m4 l4"> </div>
-	  	<div class="col s10 m4 l4">
-			<img class="responsive-img" src="login.png">
-		</div>
-	</div>
-  <div class="row">
-  	<div class="col s12 m3 l4"> </div>
-    <form name="login" class="col s12 m6 l4 indigo darken-3" action="conexion.php" method="post" enctype="multipart/form-data">
-    	<input type="Hidden" name="cifrado" value="">
-		<input type="Hidden" name="numero" value=""> 
-      <div class="row"><br />
-        <div class="input-field col s12">
-          <input id="usuario" name="usuario" type="text" class="validate">
-          <label for="usuario">Nombre de usuario</label>
-        </div>
-      </div>
-      <div class="row">
-        <div class="input-field col s12">
-          <input id="password" name="password" type="password" class="validate">
-          <label for="password">Contraseña</label>
-        </div>
-      </div>
-      <div class="row"><div class="col s11">
-      <button class="btn-floating btn-large waves-effect waves-light green right" type="submit" name="action" onClick="enviaMD5(calculaMD5())">
-	   	<i class="material-icons">done</i>
-  		</button>
-  		</div></div>
-    </form>
-  </div>
-</div>
+<?php
+$passwordC="123";
+$passwordA="321";
+$password1 = strtolower(md5($passwordC.$_POST["numero"]));
+$password2 = strtolower(md5($passwordA.$_POST["numero"]));
+$usuariopassword = strtolower($_POST["cifrado"]);
+session_start();
+if ($_POST['usuario']=="colegio" && $password1==$usuariopassword) {
+    $_SESSION['Susuario'] = $_POST["usuario"];
+    $_SESSION['Spassword'] = $passwordC;
+    echo "<body bgcolor='161D29' text='ddddff'><center><br><br><h1>Bienvenido ".$_SESSION['Susuario']."</h1><img src='leon.gif'></center></body>
+  		  	 <meta http-equiv='refresh' content='5; URL=index.php?pag=2' />";
+} 
+elseif ($_POST['usuario']=="Admincol" && $password2==$usuariopassword) {
+    $_SESSION['Susuario'] = $_POST["usuario"];
+    $_SESSION['Spassword'] = $passwordA;
+    echo "<body bgcolor='161D29' text='ddddff'><center><br><br><h1>Bienvenido ".$_SESSION['Susuario']."</h1><img src='leon.gif'></center></body>
+    <meta http-equiv='refresh' content='5; URL=index.php?pag=2' />";
+} 
+else echo "<body bgcolor='000000'><br><br><center><img src='error.gif'></center><body>
+<meta http-equiv='refresh' content='5;URL=index.php?pag=0' />";
+?>
