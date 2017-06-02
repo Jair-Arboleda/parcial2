5c01b862f0a46e11053bf265b3c3fc43f87018ed
ElkynEnriquez <efenriquezm@gmail.com>
2017-06-01 23:50:24 -0500

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,25 +1,3 @@
 <?php
-$passwordC="123";
-$passwordA="321";
-$password1 = strtolower(md5($passwordC.$_POST["numero"]));
-$password2 = strtolower(md5($passwordA.$_POST["numero"]));
-$usuariopassword = strtolower($_POST["cifrado"]);
-
-session_start();
-if ($_POST['usuario']=="colegio" && $password1==$usuariopassword) {
-    $_SESSION['Susuario'] = $_POST["usuario"];
-    $_SESSION['Spassword'] = $_POST["password"];
-    echo "<body bgcolor='161D29' text='ddddff'><center><br><br><h1>Bienvenido</h1><img src='leon.gif'></center></body>
-  		  	 <meta http-equiv='refresh' content='5; URL=index.php?pag=1' />";
-	$conex=mysqli_connect("localhost",$_POST["usuario"],$_POST["password"],"colegio");
-} 
-elseif ($_POST['usuario']=="Admincol" && $password2==$usuariopassword) {
-    $_SESSION['Susuario'] = $_POST["usuario"];
-    $_SESSION['Spassword'] = $_POST["password"];
-    echo "<body bgcolor='161D29' text='ddddff'><center><br><br><h1>Bienvenido</h1><img src='leon.gif'></center></body>
-    <meta http-equiv='refresh' content='5; URL=index.php?pag=1' />";
-	$conex=mysqli_connect("localhost",$_POST["usuario"],$_POST["password"],"colegio");
-} 
-else echo "<body bgcolor='000000'><br><br><center><img src='error.gif'></center><body>
-<meta http-equiv='refresh' content='5;URL=index.php?pag=1' />";
+$conex=mysqli_connect("localhost",$_SESSION['Susuario'],$_SESSION['Spassword'],"colegio");
 ?>
